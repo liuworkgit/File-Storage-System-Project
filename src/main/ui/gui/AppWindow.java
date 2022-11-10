759836cfbd5c2631ef8040af93f37a1bdac373e9
moses21 <moses21@student.ubc.ca>
2022-11-09 17:02:18 -0800

    Added JMenuItems to menu bar, deleted MenuBar class

--- src/main/ui/gui/AppWindow.java
+++ src/main/ui/gui/AppWindow.java
@@ -1,30 +1,33 @@
 package ui.gui;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
-import java.util.*;
 
 // the window in which the app appears
 public class AppWindow extends JFrame {
     private JFrame window;
+    private JPanel sidebar;
+    private JPanel textWindow;
+    private JMenuBar topMenuBar;
     private static final int WIDTH = 600;
     private static final int HEIGHT = 550;
 
     // EFFECTS: creates a new application window with all the app's components
     public AppWindow() {
         window = new JFrame("NoteTaker");
-        JPanel sidebar = new JPanel();
-        JPanel textWindow = new JPanel();
-        JPanel menuBar = new JPanel();
+        sidebar = new JPanel();
+        textWindow = new JPanel();
+        topMenuBar = new JMenuBar();
 
         // settings
         adjustSidebar(sidebar);
         adjustTextWindow(textWindow);
-        adjustMenuBar(menuBar);
+        adjustMenuBar(topMenuBar);
 
         // add to jframe + align
         window.setLayout(new BorderLayout());
-        window.add(menuBar, BorderLayout.PAGE_START);
+        window.add(topMenuBar, BorderLayout.PAGE_START);
         window.add(sidebar, BorderLayout.LINE_START);
         window.add(textWindow, BorderLayout.CENTER);
 
@@ -49,9 +52,29 @@ public class AppWindow extends JFrame {
         textWindow.setPreferredSize(new Dimension(450, HEIGHT - 60));
     }
 
-    // EFEFCTS: sets menu bar settings
-    public void adjustMenuBar(JPanel menuBar) {
-        menuBar.setBackground(Color.DARK_GRAY);
-        menuBar.setPreferredSize(new Dimension(WIDTH, 60));
+    // EFFECTS: sets menu bar implementation, including event handling
+    public void adjustMenuBar(JMenuBar menuBar) {
+        // create components
+        JLabel label = new JLabel("NoteTaker");
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem loadOption = new JMenuItem("Open File");
+        JMenuItem saveOption = new JMenuItem("Save File");
+        JMenuItem addFolder = new JMenuItem("Add Folder");
+        JMenuItem deleteFolder = new JMenuItem("Delete Folder");
+        JMenuItem renameFolder = new JMenuItem("Rename Folder");
+        JMenuItem addPage = new JMenuItem("Add Page");
+        JMenuItem deletePage = new JMenuItem("Delete Page");
+
+        menuBar.add(label);
+        fileMenu.add(loadOption);
+        fileMenu.add(saveOption);
+        fileMenu.addSeparator(); // separator
+        fileMenu.add(addFolder);
+        fileMenu.add(deleteFolder);
+        fileMenu.add(renameFolder);
+        fileMenu.addSeparator(); // separator
+        fileMenu.add(addPage);
+        fileMenu.add(deletePage);
+        menuBar.add(fileMenu);
     }
 }
--- src/main/ui/gui/Sidebar.java
+++
@@ -1,5 +0,0 @@
-package ui.gui;
-
-// sidebar where all folders appear
-public abstract class Sidebar {
-}
