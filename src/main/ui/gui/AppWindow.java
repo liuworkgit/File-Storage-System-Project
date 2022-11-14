14fcbf4e942bef14a0ad300e4120b7228092df01
moses21 <moses21@student.ubc.ca>
2022-11-13 18:51:36 -0800

    Coupled AppWindow with Dimension to reduce repetition of common constants, added implementation to SideBar

--- src/main/ui/gui/AppWindow.java
+++ src/main/ui/gui/AppWindow.java
@@ -7,38 +7,27 @@ import java.awt.*;
 // Link to original code: https://docs.oracle.com/javase/tutorial/uiswing/examples/components/index.html
 
 // the window in which the app appears
-public class AppWindow extends JFrame {
+public class AppWindow extends JFrame implements Dimensions {
     private JFrame window;
-    private JPanel sidePanel;
-    protected static final int WIDTH = 600;
-    protected static final int HEIGHT = 550;
+    protected static final int WIDTH = width;
+    protected static final int HEIGHT = height;
 
     // EFFECTS: creates a new application window with all the app's components
     public AppWindow() {
         window = new JFrame("NoteTaker");
-        sidePanel = new JPanel(); // TODO REPURPOSE SIDE PANEL WITH TREE
         TopMenuBar topMenuBar = new TopMenuBar();
         TextArea textArea = new TextArea();
+        SideBar sideBar = new SideBar();
 
         // add to jframe + align
         window.setLayout(new BorderLayout());
         window.setJMenuBar(topMenuBar.createMenuBar());
-        window.add(sidePanel, BorderLayout.LINE_START);
+        window.add(sideBar.createSideBar(), BorderLayout.LINE_START);
         window.add(textArea.createTextArea(), BorderLayout.CENTER);
 
         // visibility + behaviour on close
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(WIDTH, HEIGHT);
         window.setVisible(true);
-
-        // settings
-        adjustSidePanel(sidePanel);
-
-    }
-
-    // EFFECTS: sets side panel settings
-    public void adjustSidePanel(JPanel sidePanel) {
-        sidePanel.setBackground(Color.GRAY);
-        sidePanel.setPreferredSize(new Dimension(150, HEIGHT - 60));
     }
 }
--- src/main/ui/gui/SideBar.java
+++ src/main/ui/gui/SideBar.java
@@ -2,11 +2,14 @@ package ui.gui;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
 
-// Based on the Tree Demo Project 2 from the Oracle Java Documentation
+import model.Page;
+
+// Based on the Tree Demo Project from the Oracle Java Documentation
 // Link to original code: https://docs.oracle.com/javase/tutorial/uiswing/examples/components/index.html
 
 // represents the sidebar that displays folders in the application
@@ -14,30 +17,70 @@ public class SideBar implements Dimensions {
 
     // EFFECTS: creates the sidebar that displays all the folders
     public Container createSideBar() {
-        JScrollPane sidebar = new JScrollPane();
         JTree tree = createTree();
-
+        // create scroll pane and add tree to it
+        JScrollPane sidebar = new JScrollPane();
+        adjustSideBar(sidebar);
         sidebar.add(tree);
+
         return sidebar;
     }
 
     // EFFECTS: sets sidebar settings
-    public void adjustSidePanel(JPanel sidebar) {
-        sidebar.setBackground(Color.GRAY);
+    public void adjustSideBar(JScrollPane sidebar) {
+//        sidebar.setBackground(Color.GRAY); // TODO - CHANGE AFTER ALL IS DONE
         sidebar.setPreferredSize(new Dimension(150, height - 60));
     }
 
     // EFFECTS: creates a tree
     public JTree createTree() {
-        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("PLACEHOLDER NAME");
-        DefaultTreeModel treeModel = new DefaultTreeModel(rootNode);
-//        treeModel.addTreeModelListener(new createTreeModelListener());
+        // create nodes
+        DefaultMutableTreeNode root =
+                new DefaultMutableTreeNode("Directory"); // TODO - TOGGLE NAME VISIBLE OFF?
+        createTestNodes(root);
 
-        JTree tree = new JTree(treeModel);
-        tree.setEditable(true); // can change name of nodes
-        tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION); // one selection at time
-        tree.setShowsRootHandles(true); // shows node names?
+        // create single selection tree
+        JTree tree = new JTree(root);
+        tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+
+        // set icon for leaf nodes
+        // Icon is One Page icon from icons8.com
+        // link here: https://icons8.com/icon/92781/one-page
+        ImageIcon leafIcon = createImageIcon("icons/page-icon.png");
+        if (leafIcon != null) {
+            DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();
+            renderer.setLeafIcon(leafIcon);
+            tree.setCellRenderer(renderer);
+        } else {
+            System.err.println("Leaf icon missing; using default.");
+        }
+
+        // listen for when selection changes
 
         return new JTree(); // stub // TODO TREE
+    }
+
+    // EFFECTS: creates an image icon to represent leaf nodes
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = SideBar.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    // EFFECTS: creates test nodes to see if my shit works // TODO - TESTING METHOD
+    private void createTestNodes(DefaultMutableTreeNode root) {
+        DefaultMutableTreeNode category = null;
+        DefaultMutableTreeNode book = null;
+
+        category = new DefaultMutableTreeNode("test folder");
+        root.add(category);
+
+        //original Tutorial
+        book = new DefaultMutableTreeNode(new Page("test name"));
+        category.add(book);
     }
 }
